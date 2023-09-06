5fafeea885ce923a95fe16108603dae4ed747986
Kamikotosss <108273635+Kamikotosss@users.noreply.github.com>
2023-09-06 20:19:58 +0200

    info styled

--- src/components/item-identity/item-identity-info/ItemIdentityInfo.tsx
+++ src/components/item-identity/item-identity-info/ItemIdentityInfo.tsx
@@ -7,61 +7,53 @@ interface ItemIdentityInfoInterface{
 export const ItemIdentityInfo:React.FC<ItemIdentityInfoInterface> = ({identity}) => {
     const {sin1,dmgType1,dmgType2,dmgType3,guardType,idTier,imgUrl,name,passive1Tier,passive2Tier,rarity,season,sin2,sin3,sinGuard,sinner} = identity;
     const skills = [
-        {dmgType:dmgType1 ,count:3 ,sin:sin1},
-        {dmgType:dmgType2,count:2 ,sin:sin2},
-        {dmgType:dmgType3 ,count:1 , sin:sin3},
+        {imageUrl:`dmg-type/${dmgType1}` , type:dmgType1 ,count:3 ,sin:sin1},
+        {imageUrl:`dmg-type/${dmgType2}` , type:dmgType2,count:2 ,sin:sin2},
+        {imageUrl:`dmg-type/${dmgType3}` , type:dmgType3 ,count:1 , sin:sin3},
+        {imageUrl:`guard-type/${guardType}` , type:guardType ,count:1 , sin:sinGuard},
     ];
+    const passives = [
+        {imageUrl:`sins/${sin1}` , count:`x${3}` , description: "Description" , tier:passive1Tier},
+        {imageUrl:`sins/${sin1}` , count:`x${3}` , description: "Description" , tier:passive2Tier},
+        {imageUrl:`sins/${sin1}` , count:`x${3}` , description: "Description" , tier:idTier},
+    ]
     //const rarityStyled = rarity.replaceAll("O","Ø");
     return (
-        <div className={"item-identity-info-container"} 
-            style={{
-                backgroundImage: `linear-gradient(143deg, rgba(0, 0, 0, 0.40) 17.06%, rgba(0, 0, 0, 0.00) 52.01%), linear-gradient(180deg, rgba(0, 0, 0, 0.00) 10.42%, rgba(0, 0, 0, 0.60) 84.37%), url("/images/identities/${identity.imgUrl}.png")`,
-                backgroundPosition: 'center', 
-                backgroundSize: 'cover',     
-                backgroundRepeat: 'no-repeat', 
-            }}
-        >
+        <div className={"item-identity-info-container"}  >
             <div className={"item-identity-info-skills"}>
-                {skills.map((skill)=>{
+                {skills.map(({imageUrl,count,sin,type})=>{
                     return (
                         <div className="item-identity-info-sin">
-                            <img className={"item-identity-info-skill-dmgType"} src={`/images/dmg-type/${skill.dmgType}.png`}></img>
-                            {/* <span className={"item-identity-info-skill-count"} >{skill.count}</span> */}
-                            <div className={["item-identity-info-line", `item-identity-info-line--${skill.sin}`].join(" ")}></div>
+                            <div className={"item-identity-info-skill-container"}>
+                                <img className={"item-identity-info-skill-dmgType"} src={`/images/${imageUrl}.png`}></img>
+                            </div>
+                            <div className={["item-identity-info-line", `item-identity-info-line--${sin}`].join(" ")}></div>
                         </div>
                     )
                 })}
-                <div className="item-identity-info-sin">
-                            <img className={"item-identity-info-skill-dmgType"} src={`/images/guard-type/${guardType}.png`}></img>
-                            {/* <span className={"item-identity-info-skill-count"} >{skill.count}</span> */}
-                            <div className={["item-identity-info-line", `item-identity-info-line--${sinGuard}`].join(" ")}></div>
-                </div>
+                
            </div>
            <div className={"item-identity-info-tiers"}>
-                <div className="item-identity-info-tier">
-                    <div className="item-identity-info-tier-description">
-                        Личность
-                    </div>
-                    <div className="item-identity-info-tier-rank">
-                        {idTier}
-                    </div>
-                </div>
-                <div className="item-identity-info-tier">
-                    <div className="item-identity-info-tier-description">
-                        Боевая пассивка
-                    </div>
-                    <div className="item-identity-info-tier-rank">
-                        {passive1Tier}
-                    </div>
-                </div>
-                <div className="item-identity-info-tier">
-                    <div className="item-identity-info-tier-description">
-                        Саппорт пассивка
-                    </div>
-                    <div className="item-identity-info-tier-rank">
-                        {passive2Tier}
-                    </div>
-                </div>
+                {
+                    passives.map(({imageUrl,count,description,tier})=>{
+                            return(
+                            <div className="item-identity-info-tier">
+                                <div className="item-identity-info-tier-description">
+                                    <img src={`/images/${imageUrl}.png`} className="item-identity-info-tier-description-sin">
+                                    </img>
+                                    <span className="item-identity-info-tier-description" >{count}</span>
+                                </div>
+                                <div className="item-identity-info-tier-rank-container">
+                                    <span className="item-identity-info-tier-rank-description" >{description}</span>
+                                    <span className={["item-identity-info-tier-rank", `item-identity-info-tier-rank--${tier}`].join(" ")} >{tier}</span>
+                                </div>
+                            </div>
+                            )
+                    })
+                }
+            </div>
+            <div className={"item-identity-info-arrow"}>
+                
             </div>
         </div>
     )
